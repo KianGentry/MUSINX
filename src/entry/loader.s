667eed6ccf06d258ce603bc92ed2b71737033e03
Kian Gentry <musinks@musinks.com>
2026-09-19 15:09:41 +0100

    trap_handler: for non ebreak traps, write to serial

--- src/entry/loader.s
+++ src/entry/loader.s
@@ -26,11 +26,32 @@ _start:
 .align 2
 .globl trap_vector
 .type trap_vector, @function
+.extern trap_unhandled
 
 trap_vector:
+    # save return address (for C)
+    addi sp, sp, -16
+    sd ra, 8(sp)
+
+    # read trap reason
+    csrr a0, scause
+
+    # unexpected trap handling, cause 3
+    li t0, 3
+    bne a0, t0, unexpected_trap
+
+    # if ebreak, skip 4 bytes
     csrr t0, sepc
     addi t0, t0, 4
     csrw sepc, t0
+
+    # return from (only) ebreak
+    ld ra, 8(sp)
+    addi sp, sp, 16
     sret
+
+unexpected_trap:
+    # pass to C
+    call trap_unhandled
 
 .size trap_vector, . - trap_vector
--- src/kernel.c
+++ src/kernel.c
@@ -7,6 +7,15 @@ static void console_write(const char *text) {
     }
 }
 
+void trap_unhandled(unsigned long cause) {
+    (void)cause;
+    console_write("kernel: unexpected trap\n");
+
+    for (;;) {
+        wfi();
+    }
+}
+
 void kernel_main(void) {
     console_write("MUSINX\n");
     // sbi forward to debug console
